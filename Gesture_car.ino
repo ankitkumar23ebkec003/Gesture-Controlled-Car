1941388d8f31513e91a4171361ad7930815a0cff
ankitkumar14042-cell <ankitkumar14042@gmail.com>
2026-04-11 13:11:32 +0530

    Create Gesture_car.ino

---
+++ Gesture_car.ino
@@ -0,0 +1,15 @@
+# Gesture Controlled Car
+
+A car controlled using hand gestures with Arduino and accelerometer.
+
+## Technologies Used
+- Arduino
+- Accelerometer
+- Motor Driver
+
+## Description
+The car moves according to hand movements detected by the sensor.
+
+## Applications
+- Robotics
+- Automation
